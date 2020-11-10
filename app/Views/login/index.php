b13fbb228d9f1018c20a23274bd4c5ae5c695653
iqbalmirza13 <iqbalmirza1214@gmail.com>
2020-11-10 11:48:14 +0700

    login front end

--- app/Views/login/index.php
+++ app/Views/login/index.php
@@ -5,26 +5,26 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-    <link rel="icon" type="image/png" href="<?php echo base_url("images/icons/favicon.ico")?>">
+    <link rel="icon" type="image/png" href="<?php echo base_url("assets/login/images/icons/favicon.ico")?>">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/login/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href=<?php echo base_url("fonts/font-awesome-4.7.0/css/font-awesome.min.css")?>>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("assets/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css")?>>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href=<?php echo base_url("fonts/iconic/css/material-design-iconic-font.min.css")?>>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("assets/login/fonts/iconic/css/material-design-iconic-font.min.css")?>>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href=<?php echo base_url("vendor/animate/animate.css")?>>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("assets/login/vendor/animate/animate.css")?>>
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href=<?php echo base_url("vendor/css-hamburgers/hamburgers.min.css")?>>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("assets/login/vendor/css-hamburgers/hamburgers.min.css")?>>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href=<?php echo base_url("vendor/animsition/css/animsition.min.css")?>>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("assets/login/vendor/animsition/css/animsition.min.css")?>>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href=<?php echo base_url("vendor/select2/select2.min.css")?>>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("assets/login/vendor/select2/select2.min.css")?>>
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href=<?php echo base_url("vendor/daterangepicker/daterangepicker.css")?>>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("assets/login/vendor/daterangepicker/daterangepicker.css")?>>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href=<?php echo base_url("css/util.css")?>>
-	<link rel="stylesheet" type="text/css" href=<?php echo base_url("css/main.css")?>>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("assets/login/css/util.css")?>>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("assets/login/css/main.css")?>>
 <!--===============================================================================================-->
 </head>
 <body>
@@ -78,21 +78,21 @@
 	<div id="dropDownSelect1"></div>
 	
 <!--===============================================================================================-->
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src=<?php echo base_url("vendor/jquery/jquery-3.2.1.min.js")?>></script>
 <!--===============================================================================================-->
-	<script src="vendor/animsition/js/animsition.min.js"></script>
+	<script src=<?php echo base_url("vendor/animsition/js/animsition.min.js")?>></script>
 <!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src=<?php echo base_url("vendor/bootstrap/js/popper.js")?>></script>
+	<script src=<?php echo base_url("vendor/bootstrap/js/bootstrap.min.js")?>></script>
 <!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
+	<script src=<?php echo base_url("vendor/select2/select2.min.js")?>></script>
 <!--===============================================================================================-->
-	<script src="vendor/daterangepicker/moment.min.js"></script>
-	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+	<script src=<?php echo base_url("vendor/daterangepicker/moment.min.js")?>></script>
+	<script src=<?php echo base_url("vendor/daterangepicker/daterangepicker.js")?>></script>
 <!--===============================================================================================-->
-	<script src="vendor/countdowntime/countdowntime.js"></script>
+	<script src=<?php echo base_url("vendor/countdowntime/countdowntime.js")?>></script>
 <!--===============================================================================================-->
-	<script src="js/main.js"></script>
+	<script src=<?php echo base_url("js/main.js")?>></script>
 
 </body>
 </html>
